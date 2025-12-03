ef798c17512fdce18eba0fd7bc8845006c583288
marcos <marquitosgonzalez422@gmail.com>
2025-12-02 22:26:52 -0300

    agregamos el componente tería

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-function App() {
+import Teoria from "./components/Teoria";
 
+function App() {
+  // aquí iria la lógica del componente
   return (
-  <h1>hola mundo</h1>
-  )
+    // aqui también puedo agregar lógica, no es normal pero se puede agregar algo pequeño
+    <>
+      <header className="py-3 text-center">
+        <h1 className="display-4">primera app con react</h1>
+      </header>
+      <main className = "container">
+        <Teoria></Teoria>
+        {/* <Teoria></Teoria> */}
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Teoria.jsx
@@ -0,0 +1,10 @@
+const Teoria = () => {
+    return (
+        <section>
+            <h2 className = "alert alert-primary">que es react</h2>
+            <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario. Se destaca por su enfoque en la creación de aplicaciones de web de una sola página SPA y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un DOM virtual. <br /> React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
+        </section>
+    );
+};
+
+export default Teoria;
