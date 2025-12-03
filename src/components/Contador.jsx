723b24b01a6578be5af98dc8b868b360815276de
marcos <marquitosgonzalez422@gmail.com>
2025-12-03 00:06:56 -0300

    creamos el componente contador usando una variable comun

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,17 @@
 const Contador = () => {
+let numero = 1
+
+const sumar = () =>{
+    numero++
+    console.log(numero)
+
+}
     return (
         <section className="text-center">
             <h2>Contador</h2>
             <div className="d-flex justify-content-center">
-                <button className = "btn btn-primary">+</button>
-                <p className = "fs-3 mx-3">1</p>
+                <button className = "btn btn-primary" onClick={sumar}>+</button>
+                <p className = "fs-3 mx-3">{numero}</p>
                 <button className = "btn btn-danger">-</button>
             </div>
         </section>
